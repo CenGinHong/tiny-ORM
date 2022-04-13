a12d8ce0a374379fe372e8368b701bb11e0d9556
CenGinHong <55742607+CenGinHong@users.noreply.github.com>
2022-04-13 23:03:20 +0800

    fix bug

--- clause/generator.go
+++ clause/generator.go
@@ -83,7 +83,7 @@ func _update(values ...interface{}) (string, []interface{}) {
 		keys = append(keys, k+" = ?")
 		vars = append(vars, v)
 	}
-	return fmt.Sprintf("UPFATE %s SET %s", tableName, strings.Join(keys, ", ")), vars
+	return fmt.Sprintf("UPDATE %s SET %s", tableName, strings.Join(keys, ", ")), vars
 }
 
 func _delete(values ...interface{}) (string, []interface{}) {
